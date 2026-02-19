8a0e048985908b52d8ad9454157643f4d1d1b28b
Yashwanthyadav27 <yashwanthyadav323@gmail.com>
2026-02-19 17:23:56 +0530

    Update movie posters with higher resolution images

--- src/data/movies.js
+++ src/data/movies.js
@@ -2,42 +2,42 @@ export const trending = [
   {
     id: 1,
     title: "Stranger Things",
-    image: "https://image.tmdb.org/t/p/w300/49WJfeN0moxb9IPfGn8AIqMGskD.jpg"
+    image: "https://image.tmdb.org/t/p/w500/49WJfeN0moxb9IPfGn8AIqMGskD.jpg"
   },
   {
     id: 2,
     title: "The Witcher",
-    image: "https://image.tmdb.org/t/p/w300/7vjaCdMw15FEbXyLQTVa04URsPm.jpg"
+    image: "https://image.tmdb.org/t/p/w500/7vjaCdMw15FEbXyLQTVa04URsPm.jpg"
   },
   {
     id: 3,
     title: "Money Heist",
-    image: "https://image.tmdb.org/t/p/w300/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
+    image: "https://image.tmdb.org/t/p/w500/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
   },
   {
     id: 4,
     title: "Dark",
-    image: "https://image.tmdb.org/t/p/w300/apbrbWs8M9lyOpJZH5rXd7fZQia.jpg"
+    image: "https://image.tmdb.org/t/p/w500/apbrbWs8M9lyOpJZH5rXd7fZQia.jpg"
   },
   {
     id: 5,
     title: "Ozark",
-    image: "https://image.tmdb.org/t/p/w300/hNaBXLi7s7byM6SrWk2wgYyiRGU.jpg"
+    image: "https://image.tmdb.org/t/p/w500/hNaBXLi7s7byM6SrWk2wgYyiRGU.jpg"
   },
   {
     id: 6,
     title: "Bridgerton",
-    image: "https://image.tmdb.org/t/p/w300/qaewZKBKmXjb4ZfFB6KBaztT1Cj.jpg"
+    image: "https://image.tmdb.org/t/p/w500/qaewZKBKmXjb4ZfFB6KBaztT1Cj.jpg"
   },
   {
     id: 7,
     title: "Squid Game",
-    image: "https://image.tmdb.org/t/p/w300/dDlEmu3EZ0Pgg93K2SVNLCjCSvE.jpg"
+    image: "https://image.tmdb.org/t/p/w500/dDlEmu3EZ0Pgg93K2SVNLCjCSvE.jpg"
   },
   {
     id: 8,
     title: "You",
-    image: "https://image.tmdb.org/t/p/w300/7cGyv8k0Pq8NBOjV7r7Z3Q6Z5Zq.jpg"
+    image: "https://image.tmdb.org/t/p/w500/7cGyv8k0Pq8NBOjV7r7Z3Q6Z5Zq.jpg"
   }
 ];
 
@@ -45,42 +45,42 @@ export const popular = [
   {
     id: 9,
     title: "Breaking Bad",
-    image: "https://image.tmdb.org/t/p/w300/ggFHVNu6YYI5L9pCfOacjizRGt.jpg"
+    image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg"
   },
   {
     id: 10,
     title: "The Crown",
-    image: "https://image.tmdb.org/t/p/w300/1M876KPqH6J5K8G3L9w5h5Z5Zq.jpg"
+    image: "https://image.tmdb.org/t/p/w500/1M876KPqH6J5K8G3L9w5h5Z5Zq.jpg"
   },
   {
     id: 11,
     title: "Narcos",
-    image: "https://image.tmdb.org/t/p/w300/rTmal9fDbwh5F0waol2hq35U8d.jpg"
+    image: "https://image.tmdb.org/t/p/w500/rTmal9fDbwh5F0waol2hq35U8d.jpg"
   },
   {
     id: 12,
     title: "Black Mirror",
-    image: "https://image.tmdb.org/t/p/w300/5UaYsGZOFhjFDwQh6GuLjjA1Wl.jpg"
+    image: "https://image.tmdb.org/t/p/w500/5UaYsGZOFhjFDwQh6GuLjjA1Wl.jpg"
   },
   {
     id: 13,
     title: "Mindhunter",
-    image: "https://image.tmdb.org/t/p/w300/fbKE87mojpIETWepSbD5Qt741fp.jpg"
+    image: "https://image.tmdb.org/t/p/w500/fbKE87mojpIETWepSbD5Qt741fp.jpg"
   },
   {
     id: 14,
     title: "The Queen's Gambit",
-    image: "https://image.tmdb.org/t/p/w300/zU0htwvhNvCkID4Xl9fC6q4y7.jpg"
+    image: "https://image.tmdb.org/t/p/w500/zU0htwvhNvCkID4Xl9fC6q4y7.jpg"
   },
   {
     id: 15,
     title: "House of Cards",
-    image: "https://image.tmdb.org/t/p/w300/hKWxWjFwnM0f6d5X8q8Z8Z8Z8Z.jpg"
+    image: "https://image.tmdb.org/t/p/w500/hKWxWjFwnM0f6d5X8q8Z8Z8Z8Z.jpg"
   },
   {
     id: 16,
     title: "Peaky Blinders",
-    image: "https://image.tmdb.org/t/p/w300/6PX0r5TRr5g5y3V5V5V5V5V5V.jpg"
+    image: "https://image.tmdb.org/t/p/w500/6PX0r5TRr5g5y3V5V5V5V5V5V.jpg"
   }
 ];
 
@@ -88,42 +88,42 @@ export const topRated = [
   {
     id: 17,
     title: "Inception",
-    image: "https://image.tmdb.org/t/p/w300/9gk7adHYeDvHkCSEqAvQNLV5U.jpg"
+    image: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5U.jpg"
   },
   {
     id: 18,
     title: "The Dark Knight",
-    image: "https://image.tmdb.org/t/p/w300/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
   },
   {
     id: 19,
     title: "Interstellar",
-    image: "https://image.tmdb.org/t/p/w300/gEU2QniL6C8z19uVOtYnZ5UYj.jpg"
+    image: "https://image.tmdb.org/t/p/w500/gEU2QniL6C8z19uVOtYnZ5UYj.jpg"
   },
   {
     id: 20,
     title: "Pulp Fiction",
-    image: "https://image.tmdb.org/t/p/w300/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    image: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
   },
   {
     id: 21,
     title: "The Shawshank Redemption",
-    image: "https://image.tmdb.org/t/p/w300/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+    image: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
   },
   {
     id: 22,
     title: "Forrest Gump",
-    image: "https://image.tmdb.org/t/p/w300/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg"
+    image: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg"
   },
   {
     id: 23,
     title: "The Godfather",
-    image: "https://image.tmdb.org/t/p/w300/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+    image: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
   },
   {
     id: 24,
     title: "Fight Club",
-    image: "https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+    image: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
   }
 ];
 
